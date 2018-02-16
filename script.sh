9dbe4e3c78a32050f4e87d2d7f6d8461b7ba44d0
jdgasser <35599913+jdgasser@users.noreply.github.com>
2018-02-16 11:12:56 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
-﻿
 #installation des clés publiques/privées
 eval `ssh-agent -s`
 chmod 0600 /root/key_rsa
@@ -6,12 +5,12 @@ ssh-add /root/key_rsa
 rm -rf /root/key_rsa
 	
 #Git en master
-mkdir /home/server_suivi
-cd /home/server_suivi
+mkdir $HOME_GIT
+cd $HOME_GIT
 git init
-git pull ssh://git@10.1.152.219:10022/InformationSystemNetworkandCloud/Network-system/Suivi_etudiant/suivi_etudiant.git
+git pull $URL_GIT
 
-sh /home/server_suivi/install/install.sh
+sh $HOME_GIT/install/install.sh
 
 rm -rf /root/script.sh
 touch /root/script.sh
